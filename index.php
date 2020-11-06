f0e59c242e03e326bf644700c998be62c82192f0
larkingz01 <73679235+larkingz01@users.noreply.github.com>
2020-11-06 15:11:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="img/staffs/staff4.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Our Staff</h4>
+                    <p class="card-text"><small class="text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="img/staffs/staff2.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Our Staff</h4>
+                    <p class="card-text"><small class="text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+            <div class="card">
+                <img class="card-img-top" src="img/staffs/staff1.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Our Staff</h4>
+                    <p class="card-text"><small class="text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</small></p>
+                    </div>
+                </div>
+        </div>
+    </div>
+                
+
+
+</body>
+</html>
+?>
